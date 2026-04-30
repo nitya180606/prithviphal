7648b5934c3c7a328657e6f230bd50791fa7f5a0
Nithya <nithya.pabba@gmail.com>
2026-04-30 23:20:28 +0530

    remove debug log

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,4 +1,3 @@
-console.log("PK START:", JSON.stringify(process.env.FIREBASE_PRIVATE_KEY?.substring(0, 60)));
 require("dotenv").config();
 const Groq = require("groq-sdk");
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
